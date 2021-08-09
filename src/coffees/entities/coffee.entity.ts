4dfa068f060d4d7cdb1a8ff116dc54126066c2ff
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-09 20:16:01 +0200

    Edit coffee.entity to convert class to a db entity. We can check it with adminer => table is automatically created

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('coffees') // sql table will be `coffees`. Without argument, it will be class name in lowercase === 'coffee'.
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   origin: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
