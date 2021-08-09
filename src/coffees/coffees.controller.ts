2f40a491307815df90fb232de388323583f59809
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-09 11:03:45 +0200

    First method to dynamic params

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   getCoffees() {
     return 'List of coffees';
+  }
+
+  @Get(':id')
+  getCoffee(@Param() params) {
+    return `This route return coffee #${params.id}`;
   }
 }
