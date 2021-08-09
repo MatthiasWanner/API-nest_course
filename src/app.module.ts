0bab2b4a00ec79fdbd3e0cc32519e3dd2bb955a5
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-09 19:56:33 +0200

    Write db config in .env file. Import ConfigModule and TypeOrmModule in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    ConfigModule.forRoot(), // Config module is required to read environment variables
+    TypeOrmModule.forRoot({
+      type: 'postgres', // type of our database
+      host: process.env.DB_HOST, // database host
+      port: +process.env.DB_PORT, // database host
+      username: process.env.DB_USER, // username
+      password: process.env.DB_PASSWORD, // user password
+      database: process.env.DB_NAME, // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
