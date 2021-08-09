0484fa95474c6d13f2469d2103b3bf64da2222b2
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-09 15:04:12 +0200

    Deport business logic in service file

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
 
+import { Coffee } from './entities/coffee.entity';
+
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Cappuccino',
+      origin: 'Brazil',
+      flavors: ['chocolate', 'vanilla'],
+    },
+    {
+      id: 2,
+      name: 'Cafe Mocha',
+      origin: 'Colombia',
+      flavors: ['espresso', 'mocha'],
+    },
+  ];
+
+  findAll(): Coffee[] {
+    return this.coffees;
+  }
+
+  findOne(id: string): Coffee {
+    return this.coffees.find((coffee) => coffee.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      return { message: `Coffee #${id} updated`, body: updateCoffeeDto };
+    } else {
+      return { message: `Coffee ${id} not found` };
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
---
+++ src/coffees/entities/coffee.entity.ts
@@ -0,0 +1,6 @@
+export class Coffee {
+  id: number;
+  name: string;
+  origin: string;
+  flavors: string[];
+}
