71a15a5a096b7962cf3d8ccf2bd917fd44fe26d3
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-10 11:37:49 +0200

    Example of Dynamic module with options passed as parameters

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,29 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { createConnection, ConnectionOptions } from 'typeorm';
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+  providers: [
+    //Initial connection configuration example
+    {
+      provide: 'CONNECTION',
+      useValue: createConnection({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+      }),
+    },
+  ],
+})
+export class DatabaseModule {
+  static register(options: ConnectionOptions): DynamicModule {
+    return {
+      module: DatabaseModule,
+      providers: [
+        {
+          provide: 'CONNECTION', // 👈
+          useValue: createConnection(options),
+        },
+      ],
+    };
+  }
+}
