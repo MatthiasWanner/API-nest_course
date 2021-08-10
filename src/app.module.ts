d1ddbb59ab22674a6f6e9fb1fef12800b7e9254a
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-10 12:49:42 +0200

    Config Joi validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     CoffeesModule,
     CoffeeRatingModule,
-    ConfigModule.forRoot(), // Config module is required to read environment variables
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().default(5432),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      }),
+    }), // Config module is required to read environment variables
     TypeOrmModule.forRoot(),
   ],
   controllers: [AppController],
