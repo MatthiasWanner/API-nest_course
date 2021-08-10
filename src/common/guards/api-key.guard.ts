6b65275ef31b4b3a211d59a085ae523f1eed51bd
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-10 20:05:59 +0200

    Example of guard handler. Check the Authorization header

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,4 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -6,6 +7,8 @@ export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return false;
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+    return authHeader === process.env.API_KEY; // Pass {header: {"Authorization": "kMnRFQ1OJcjjPdIufziiufClAtboFgp8QqIK7aCE"}} in your request to return true, if not, guard block ⛔️
   }
 }
