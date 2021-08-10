d78d647e8ea5fc0eb3e0ceccbcc351aaa6679493
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-10 11:29:50 +0200

    Create new Dynamic module using CLI

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     CoffeesModule,
     CoffeeRatingModule,
     ConfigModule.forRoot(), // Config module is required to read environment variables
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot(), DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
