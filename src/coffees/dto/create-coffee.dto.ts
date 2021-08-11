6e9472060eced098af12c8fdf082cb2754794e22
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-11 13:13:28 +0200

    Add @ApiProperty decorator with arguments to improve manually swagger doc

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'Name of the coffee' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'Origin of the coffee' })
   @IsString()
   readonly origin: string;
 
+  @ApiProperty({ description: 'Flavors array' })
   @IsString({ each: true })
   readonly flavors: string[];
 }
