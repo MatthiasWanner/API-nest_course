5cae7dcde331d3772ba2ad767a22c7a004834975
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-11 16:15:57 +0200

    Write first test logic

--- test/coffees/coffees.e2e-spec.ts
+++ test/coffees/coffees.e2e-spec.ts
@@ -1,9 +1,27 @@
-import { HttpServer, INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  HttpServer,
+  HttpStatus,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { TestingModule, Test } from '@nestjs/testing';
 import { CoffeesModule } from '../../src/coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
 
 describe('[Feature] Coffees - /coffees', () => {
+  const coffee = {
+    name: 'Shipwreck Roast',
+    origin: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+  const expectedPartialCoffee = jasmine.objectContaining({
+    // TODO: fix ReferenceError: jasmine is not defined
+    ...coffee,
+    flavors: jasmine.arrayContaining(
+      coffee.flavors.map((name) => jasmine.objectContaining({ name })),
+    ),
+  });
   let app: INestApplication;
   let httpServer: HttpServer;
 
@@ -40,7 +58,16 @@ describe('[Feature] Coffees - /coffees', () => {
     httpServer = app.getHttpServer();
   });
 
-  it.todo('Create [POST /]');
+  it('Create [POST /]', () => {
+    return request(httpServer)
+      .post('/coffees')
+      .send(coffee)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        expect(body).toEqual(expectedPartialCoffee);
+      });
+  });
+
   it.todo('Get all [GET /]');
   it.todo('Get one [GET /:id]');
   it.todo('Update one [PATCH /:id]');
