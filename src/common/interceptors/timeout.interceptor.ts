67b027b7187df4963bf6cc31dacfe3e87d7f0526
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-11 09:55:21 +0200

    Example of interceptor that check response time.

---
+++ src/common/interceptors/timeout.interceptor.ts
@@ -0,0 +1,14 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, timeout } from 'rxjs';
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(timeout(3000)); // this method will throw an error if the response takes more than 3 seconds
+  }
+}
