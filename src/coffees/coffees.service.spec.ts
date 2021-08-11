c740cc0d521d9d2e667acb49871583d65896450c
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-11 14:46:48 +0200

    Fix test error writing a mock Repository

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,27 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+const createMockRepository = <T = any>(): MockRepository<T> => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+});
+
 describe('CoffeesService', () => {
   let service: CoffeesService;
+  let coffeeRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CoffeesService,
         { provide: Connection, useValue: {} },
-        { provide: getRepositoryToken(Flavor), useValue: {} }, // 👈
-        { provide: getRepositoryToken(Coffee), useValue: {} }, // 👈
-        { provide: getRepositoryToken(Event), useValue: {} }, // 👈},
+        {
+          provide: getRepositoryToken(Flavor),
+          useValue: createMockRepository(),
+        }, // 👈
+        {
+          provide: getRepositoryToken(Coffee),
+          useValue: createMockRepository(),
+        }, // 👈
+        {
+          provide: getRepositoryToken(Event),
+          useValue: createMockRepository(),
+        }, // 👈},
       ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
+    coffeeRepository = module.get<MockRepository>(getRepositoryToken(Coffee));
   });
 
   it('should be defined', () => {
@@ -31,9 +48,10 @@ describe('CoffeesService', () => {
   describe('findOne', () => {
     describe('when the coffee exists', () => {
       it('should return one coffee', async () => {
-        const coffeeId = '1';
+        const coffeeId = '5'; // Check in your db an id that exists
         const expectedCoffee = {};
 
+        coffeeRepository.findOne.mockReturnValue(expectedCoffee);
         const foundCoffee = await service.findOne(+coffeeId);
 
         expect(foundCoffee).toEqual(expectedCoffee);
