f2df21be65ad06f2c456d69dc69410db99199e25
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-11 11:14:45 +0200

    Create new cutom middleware with 'nest g middleware CLI'. Configure it in common module to indicate in wich route apply it (* = all)

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 import { ApiKeyGuard } from './guards/api-key.guard';
+import { LoggingMiddleware } from './middlewares/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
@@ -11,4 +12,8 @@ import { ApiKeyGuard } from './guards/api-key.guard';
     { provide: APP_FILTER, useClass: HttpExceptionFilter },
   ],
 })
-export class CommonModule {}
+export class CommonModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
