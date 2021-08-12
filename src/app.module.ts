f32b6f291ff927541c53db631653047a78ec1fc7
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-12 09:34:55 +0200

    Write db url in .env. Configure DB connection in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DB_URL),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
