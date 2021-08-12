8c015b20970386c024c6202a9f3815718fd21a92
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-12 09:51:30 +0200

    Update Coffee entity to write it as Mongoose Model (id is not necessary beacaus it will generated automatically by mongo)

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,14 @@
-export class Coffee {
-  id: number;
+import { Prop, Schema } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Coffee extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   origin: string;
+
+  @Prop([String])
   flavors: string[];
 }
