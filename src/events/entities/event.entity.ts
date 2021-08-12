c0d5fe30a0b500357a0706946187d7d113788411
MatthiasWanner <matthias.wanner@gmail.com>
2021-08-12 12:27:59 +0200

    Create an index in event entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -6,11 +6,12 @@ export class Event extends Document {
   @Prop()
   type: string;
 
-  @Prop()
+  @Prop({ index: true })
   name: string;
 
-  @Prop(SchemaTypes.Mixed)
+  @Prop({ type: SchemaTypes.Mixed })
   payload: Record<string, any>;
 }
 
 export const EventSchema = SchemaFactory.createForClass(Event);
+EventSchema.index({ name: 1, type: -1 });
